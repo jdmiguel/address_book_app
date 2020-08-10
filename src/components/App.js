885f7b07f10758254f7dc61c99f0a7b95089ebce
jdmiguel <jaimedmiguel@gmail.com>
2020-08-10 13:34:48 +0100

    add card component, update mixins and styles, add image flags

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
 import Layout from './Layout';
-import PageTitle from './PageTitle';
+import Card from './Card';
+
+import pathImg from '../assets/img/spain-flag.png';
+
+const cardImgSrc = 'https://randomuser.me/api/portraits/med/women/52.jpg';
+const cardData = {
+  cardFirstLine: 'Alba Caballero',
+  cardSecondLine: 'purpleelephant787',
+  cardThirdLine: 'alba.caballero@example.com',
+};
 
 const App = () => (
   <Layout>
-    <PageTitle text="Contact list" />
+    <div className="container">
+      <div className="row">
+        <Card
+          id="28443430-601b-4608-a0ef-db12c062a3ce"
+          imgSrc={cardImgSrc}
+          data={cardData}
+          onClick={(id) => {
+            console.log(id);
+          }}
+          isActive
+        />
+        <Card
+          id="spanish"
+          imgSrc={pathImg}
+          data="Spanish"
+          onClick={(id) => {
+            console.log(id);
+          }}
+          isHighlight
+        />
+      </div>
+    </div>
   </Layout>
 );
 
