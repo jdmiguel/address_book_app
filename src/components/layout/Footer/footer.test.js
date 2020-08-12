2b453225fcf0d440625f13419ed4e02c91308cbc
jdmiguel <jaimedmiguel@gmail.com>
2020-08-12 12:40:33 +0100

    update footer and header unit tests

--- src/components/layout/Footer/footer.test.js
+++ src/components/layout/Footer/footer.test.js
@@ -1,29 +1,46 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import { render } from '@testing-library/react';
 
 import Footer from '.';
 
 describe('Component: Footer', () => {
   it('should render with home link', () => {
-    const { container } = render(<Footer />);
+    const { container } = render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>,
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render with settings link', () => {
-    const { container } = render(<Footer withSettings />);
+    const { container } = render(
+      <MemoryRouter>
+        <Footer withSettings />
+      </MemoryRouter>,
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render with home link and warning msg', () => {
-    const { container } = render(<Footer withWarning />);
+    const { container } = render(
+      <MemoryRouter>
+        <Footer withWarning />
+      </MemoryRouter>,
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render with settings link and warning msg', () => {
-    const { container } = render(<Footer withSettings withWarning />);
+    const { container } = render(
+      <MemoryRouter>
+        <Footer withSettings withWarning />
+      </MemoryRouter>,
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
